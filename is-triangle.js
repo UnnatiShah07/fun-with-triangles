ad7403ff9c8cfdef616064b6ba00d58bfb9d9d63
UnnatiShah07 <76685572+UnnatiShah07@users.noreply.github.com>
2022-09-30 21:25:40 +0530

    Complete is triangle part

---
+++ is-triangle.js
@@ -0,0 +1,24 @@
+const inputs = document.querySelectorAll(".angle-input");
+const isTriangleBtn = document.querySelector("#is-triangle-btn");
+const output = document.querySelector("#output");
+
+function calculateSumOfAngles(angle1, angle2, angle3) {
+    const sumOfAngles = angle1 + angle2 + angle3;
+    return sumOfAngles;
+}
+
+function isTriangle() {
+    const sumOfAngles = calculateSumOfAngles(
+        Number(inputs[0].value),
+        Number(inputs[1].value),
+        Number(inputs[2].value)
+    );
+    if (sumOfAngles === 180) {
+        output.innerText = "Yay, The angles form a triangle 🔺";
+    } else {
+        if (sumOfAngles === 0) output.innerText = "Please enter all fields ☹️";
+        else output.innerText = "Oh! , The angles does not form a triangle 🤪";
+    }
+}
+
+isTriangleBtn.addEventListener("click", isTriangle);
